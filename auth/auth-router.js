d06f12e5f7ebc4da6b482202245af4ca7084a16d
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-25 17:17:53 -0600

    compareSync functioning on login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -14,7 +14,30 @@ router.post('/register', (req, res) => {
         .catch(error => res.send(error))
 })
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                console.log('db password', user.password)
+                console.log('login password', password)
+                res.status(200).json({ message: `Welcome ${user.username}` })
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json(error)
+        })
+})
+
 
 
 
 module.exports = router;
+
+
+
