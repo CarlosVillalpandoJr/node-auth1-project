7b58e9cea053a44c6280e1caf07385239a663597
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-25 15:55:12 -0600

    registering user working, savedUser is not coming back in response body

--- api/api-router.js
+++ api/api-router.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const authRouter = require('../auth/auth-router');
 const userRouter = require('../users/user-router');
 
-router.use('/users', userRouter)
+router.use('/auth', authRouter);
+router.use('/users', userRouter);
+
 
 router.get('/', (req, res) => {
     res.json({ api: "Api router running" })
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const Users = require('../users/user-model');
 
+router.post('/register', (req, res) => {
+    let user = req.body
+
+    Users.add(user)
+        .then(savedUser => {
+            res.status(201).json({ message: "user register", savedUser})
+        })
+        .catch(error => res.send(error))
+})
 
 
 
