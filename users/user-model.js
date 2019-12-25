2c5e15abed96f1ae5a250735e32ac326d2456c27
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-25 15:23:37 -0600

    added findBy to user-model

--- users/user-model.js
+++ users/user-model.js
@@ -1,10 +1,17 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-    find
+    find,
+    findBy
 }
 
 
 function find() {
     return db("users").select("id","username")
+}
+
+function findBy(filter) {
+    return db("users")
+        .select("id", "username", "password")
+        .where(filter)
 }
