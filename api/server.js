f2399f473ac7e6374f0f72fe7635328a77ebfda6
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2020-01-02 20:04:12 -0600

    set up sessionConfig in server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const session = require('express-session')
 
 const apiRouter = require('./api-router.js');
 const configureMiddleware = require('./configure-middleware');
 
 const server = express();
+
+const sessionConfig = {
+    name: 'session', // session id
+    secret: 'super secret key', // encryption key, for production should not be hardcoded 
+    cookie: {
+        maxAge: 1000 * 60 * 60, // session expiration
+        secure: false, // would want to be true in real world (https)
+        httpOnly: true // browser can't access via javascript
+    },
+    resave: false,
+    saveUninitialized: false 
+}
 
 configureMiddleware(server);
 
